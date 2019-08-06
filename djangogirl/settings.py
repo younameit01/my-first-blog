da99b2e948c959aa323fa8a06d9a4bb6497bbdff
aakashrock0011@gmail.com <aakashrock0011@gmail.com>
2019-08-06 12:10:54 +0530

    Adding postgresql as database

--- djangogirl/settings.py
+++ djangogirl/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'djangogirl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btwzgiax',
+        'HOST': 'raja.db.elephantsql.com',
+        'USER': 'btwzgiax',
+        'PASSWORD': '6a2SJLKR68Fop75LxJsSXalHuxUAI_yA',
+        'PORT': '5432',
     }
 }
 
